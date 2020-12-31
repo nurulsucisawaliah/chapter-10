9a475802bf344bb7aea64ab93a35ede424337754
nurulsucisawaliah <71654996+nurulsucisawaliah@users.noreply.github.com>
2020-12-31 20:26:49 +0800

    Create latihan7.py

---
+++ latihan7.py
@@ -0,0 +1,28 @@
+# membuka file
+buka = open("d:\hasilPenyandian", 'r')
+#menginput nilai n
+n = int(input('nilai n : '))
+#membaca file
+baca = buka.read()
+baca_list = list(baca)
+isi = []
+
+for m in baca_list :
+    if (m == ' ') :
+        x = ord(m)
+    else :
+        i = ord(m)
+        x = i - n
+        if (x < 65) :
+            x = x + 26
+        elif (90 < x and x <97) :
+            x = x + 26
+    kars = chr(x)
+    isi.append(kars)
+joined = ','.join(isi)
+
+#menambahkan file
+buka_open = open('latihan7.txt', 'w')
+buka_open.write(joined)
+buka_open.close()
+    
